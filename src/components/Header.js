04bb6c00c1822fbe7d97b262e7b86f44b8c17a52
Dana <28915073+mcmu21058@users.noreply.github.com>
2019-08-06 23:12:54 -0400

    Changed links

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,19 +14,28 @@ function Header() {
           <div>
           <Navbar color = "dark" dark fixed="top" expand="sm">
           <NavbarBrand href ="/">
-          <span className = "text-warning" > bight </span> 
+          <span className = "text-warning" >Better Bus App </span> 
            </NavbarBrand> 
            <NavbarToggler onclick = {() => {}}/> 
           <Collapse isOpen = {false}navbar >
           <Nav className = "ml-auto" navbar>
-          <NavItem>
+          {/* <NavItem>
                <NavLink href ="/components/">Components</NavLink>
           </NavItem>
           <NavItem>
                <NavLink href ="https://github.com/reactstrap/reactstrap">
                Shipments
                </NavLink>
+               
+          </NavItem> */}
+          <NavItem>
+               <NavLink href ="/components/">Plan Your Trip</NavLink>
           </NavItem>
+          <NavItem>
+               <NavLink href ="https://github.com/reactstrap/reactstrap">
+             Next Bus?
+               </NavLink>
+           </NavItem>
           </Nav> 
           </Collapse>
           </Navbar>
