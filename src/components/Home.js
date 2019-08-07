f75eaa51885ea8c91273622414833ca640c7b6aa
Dana <28915073+mcmu21058@users.noreply.github.com>
2019-08-06 23:25:13 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,13 @@ import React from 'react';
 function Home (){
      return (
      <div className = "home-page">
-     <h1 className = "home-title">
+     {/* <h1 className = "home-title">
           Taste Adventure one bight at a time 
+     </h1> */}
+     <h1 className = "home-title">
+          Plan Your Bus Trip
      </h1>
-     <h4 className = "home-subtitle">Inventory Manager</h4>
+     <h4 className = "home-subtitle">Would you like to use your current location?</h4>
      </div>
      )
 }
